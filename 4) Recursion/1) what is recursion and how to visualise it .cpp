9b7055b7057d585a946de7007d463a97f16045ad
Naman goyal <naman.eclipse@gmail.com>
2025-04-02 12:39:41 +0530

    Update 1) what is recursion and how to visualise it .cpp

--- 4) Recursion/1) what is recursion and how to visualise it .cpp
+++ 4) Recursion/1) what is recursion and how to visualise it .cpp
@@ -1,1 +1,9 @@
+Recursion is maethod in which a function calls itself again and again upto when all the functions in function call stack gets completely executed.
 
+There are two ways to visualize Recursion one is by visualizing recursion tree as taught by SANDEEP JAIN SIR (always visualize recursive tree in that way as that is the best way to completely visualize what is 
+happening in the Recursion .
+
+Another method to visualize Recusrion is ....believe in the function .... believe that if my machine can solve the problem for n-1 steps , for sure it will solve for n th step also ..keep a faith in it ...
+Do not try to  Visualize the complete codebbase out there , just remember one thing that trust is important.
+
+Never fail to understand the base case properly , always write the correct base case and what will happen when that base condition will be executed , you will get the correct result.
