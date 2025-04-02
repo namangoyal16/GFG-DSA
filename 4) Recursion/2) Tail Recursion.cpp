c7a9c075633e53eca28ba166129f7eca30eb96bd
Naman goyal <naman.eclipse@gmail.com>
2025-04-02 13:02:59 +0530

    Update and rename 2) Tail Recursion to 2) Tail Recursion.cpp

---
+++ 4) Recursion/2) Tail Recursion.cpp
@@ -0,0 +1,3 @@
+Tail Recursion happens when function doesn't have anything more to execute after its child function is completely executed ... Tail recursive function has less time complexity in modern compilers ..
+
+The last thing which happens in a tail recursion is to execute the last recursice call.
