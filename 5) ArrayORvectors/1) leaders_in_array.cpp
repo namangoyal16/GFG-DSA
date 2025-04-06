c8c58945adfb316d856a9b3a7e4374de327aed5e
Naman goyal <naman.eclipse@gmail.com>
2025-04-07 00:43:40 +0530

    Update 1) leaders_in_array.cpp

--- 5) ArrayORvectors/1) leaders_in_array.cpp
+++ 5) ArrayORvectors/1) leaders_in_array.cpp
@@ -1,6 +1,7 @@
 Simple problem just return index of the largest element .
 
   CODE:
+  
   #include <bits/stdc++.h>
 using namespace std;
 
@@ -33,6 +34,45 @@ int main(){
 
     return 0;
 }
+
+Recursive Solution : Same time complexity as before but extra space complexity which makes it non optimal solution.
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int givelargest(vector<int>&arr,int index){
+
+    if(index==arr.size()-1){
+        return arr.size()-1;
+    }
+
+    int med=givelargest(arr,index+1);
+
+    if(arr[index]>arr[med]){
+        return index;
+    }
+    else{
+        return med;
+    }
+}
+
+int main(){
+    
+    int n;
+    cin>>n;
+    vector<int>arr(n);
+
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+
+   int ans = givelargest(arr,0);
+
+   cout<<ans;
+
+    return 0;
+}
+
 
 Time Complexity of the code : O(n); // we are iterating over a complete array to find the largest element.
 Space complexity : O(1)
