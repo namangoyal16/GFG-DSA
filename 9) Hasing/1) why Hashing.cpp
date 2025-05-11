c04a47c35b5a86b76b26b548c0b0f4607656fa28
Naman goyal <naman.eclipse@gmail.com>
2025-05-11 15:25:33 +0530

    Update 1) why Hashing.cpp

--- 9) Hasing/1) why Hashing.cpp
+++ 9) Hasing/1) why Hashing.cpp
@@ -1,1 +1,9 @@
+Arrays are costlier data structures in terms of storing and deleting the elements in them , it takes O(log n ) time if array is sorted and insertion takes O(n) time to insert an element at its correct position
+so arrays become inefficient data structure if we talk in terms of time operation cost and hence we need other data structures which can do this task efficiently and solve our major problem of high time .
 
+Certain other data structures are : 
+Set , unordered_set ===> stores unique values in sorted manner ( only set ) and takes time as O(logn) and unordered_set stores unique values in unorderd manner in O(1) time .
+map , unordered_map ===> stores unique key value pairs in sorted order (only map) and takes time as O(logn) and unordered_map stores unique key value pairs in unordered way in O(1) time.
+
+there is multiset also which can store multiple values in a set in O(logn) time .
+  
